3c5d61035f900bc8152eeca7776ff522014737b3
seungheon <77201925+peaceseungheon@users.noreply.github.com>
2024-12-06 23:52:34 +0900

    feat: 로그인 화면 라우팅 설정

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,11 @@ const router = createRouter({
       path: '/signup',
       name: 'signup',
       component: () => import('@/views/SignupView.vue')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/LoginView.vue')
     }
   ]
 })
