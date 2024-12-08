6bc5be2a6f781ba653d5d561cb56950ea565aa9a
seungheon <77201925+peaceseungheon@users.noreply.github.com>
2024-12-09 00:40:27 +0900

    refactor: 불필요한 style 태그 삭제

--- src/App.vue
+++ src/App.vue
@@ -11,22 +11,3 @@ import NavigationBar from '@/components/NavigationBar.vue'
     </main>
   </div>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-.router-link-active {
-  color: #2563eb;
-  font-weight: bold;
-}
-</style>
