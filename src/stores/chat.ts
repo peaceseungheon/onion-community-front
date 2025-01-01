d073f1dc7e6b51ee3b5cf57eed48c9fa227d6971
seungheon <77201925+peaceseungheon@users.noreply.github.com>
2025-01-01 16:34:18 +0900

    stomp 통신 pub/sub 경로 수정

--- src/stores/chat.ts
+++ src/stores/chat.ts
@@ -3,24 +3,18 @@ import { Client } from "@stomp/stompjs"
 import type { IChatMessage } from '@/interface/chat'
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
+import { useAuthStore } from './auth'
 
 export const useChatStore = defineStore('chat', () => {
 
     const router = useRouter()
+    const authStore = useAuthStore()
 
     const client = new Client({
         brokerURL: "ws://localhost:8080/ws-stomp",
     })
 
     const messages = ref<IChatMessage[]>([]);
-    
-    client.onConnect = ()=> {
-        console.log('Connected ...')
-    
-        client.subscribe('/topic/greetings', (message)=> {
-            messages.value.push(JSON.parse(message.body))
-        })
-    }
     
     client.onWebSocketError = (error) => {
         alert('로그인을 먼저 해주세요.')
@@ -32,7 +26,17 @@ export const useChatStore = defineStore('chat', () => {
         console.error('Additional details: ' + frame.body);
     };
 
-    const connect = () => {
+    const connect = (chatRoomId: number) => {
+        client.onConnect = ()=> {
+            console.log('Connected ...')
+        
+            client.subscribe(`/sub/${chatRoomId}`, (message)=> {
+                const newMessage = JSON.parse(message.body)
+                if(authStore.user.userNo !== newMessage.userNo){
+                    messages.value.push(newMessage)
+                }
+            })
+        }
         client.activate()
     }
 
@@ -43,7 +47,7 @@ export const useChatStore = defineStore('chat', () => {
     const chat = (message: IChatMessage) => {
         messages.value.push(message)
         client.publish({
-            destination: '/app/hello',
+            destination: '/pub/1',
             body: JSON.stringify(message)
         })
     }
